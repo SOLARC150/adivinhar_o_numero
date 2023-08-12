0c6cd1c3753d72130c6342925737aa032f0309f1
SOLARC150 <150carlos1@gmail.com>
2023-08-12 13:42:00 -0400

    commit inicial

---
+++ src/br/com/carlos/Main.java
@@ -0,0 +1,22 @@
+package br.com.carlos;
+
+import java.util.Scanner;
+
+public class Main {
+
+	static Scanner l = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Digite um numero e irei adivinhar");
+		int a = l.nextInt();
+		
+		Integer b = a;
+		
+		
+		System.out.println("O numero que você digitou foi "+ b);
+		
+		
+	}
+	
+}
